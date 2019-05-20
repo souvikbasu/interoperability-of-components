1ab921b1f3c36a554da0375c3478a97b6b75376e
Souvik Basu <souvikbasu@gmail.com>
2019-05-20 10:46:08 +0530

    style angular app in 04

--- 04-react-in-angular-using-iframe/src/app/app.component.ts
+++ 04-react-in-angular-using-iframe/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'react-in-angular-using-iframe';
+  title = 'angular-sample-app';
 }
--- 04-react-in-angular-using-iframe/src/app/app.module.ts
+++ 04-react-in-angular-using-iframe/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularComponentComponent } from './angular-component/angular-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularComponentComponent
   ],
   imports: [
     BrowserModule
