59c6e0506da2e41f2ab7f2bdb8d543cf19ce0eac
Souvik Basu <souvikbasu@gmail.com>
2019-05-20 18:28:15 +0530

    #04 add react app build within an iframe in angular app

---
+++ 04-react-in-angular-using-iframe/src/assets/react-app/precache-manifest.c43fd4b1de1a4fdb903463728b476a9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b816b8f0b3f763361c2cf19dd1de66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "903201cb05d410688df8",
+    "url": "/static/css/main.b9d7f5e1.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "903201cb05d410688df8",
+    "url": "/static/js/main.a1ec71de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
