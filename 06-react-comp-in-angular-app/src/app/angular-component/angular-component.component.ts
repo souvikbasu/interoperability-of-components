c203bf96c3f87f80ed0647311c884903ba818f0e
Souvik Basu <souvikbasu@gmail.com>
2019-05-21 06:10:21 +0530

    add empty project to show react comp in angular app

---
+++ 06-react-comp-in-angular-app/src/app/angular-component/angular-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular-component',
+  templateUrl: './angular-component.component.html',
+  styleUrls: ['./angular-component.component.css']
+})
+export class AngularComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
