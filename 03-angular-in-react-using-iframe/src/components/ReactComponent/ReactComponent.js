7238db275fdfd567ecf12eb456f7eb4f7dac1376
Souvik Basu <souvikbasu@gmail.com>
2019-05-21 07:12:55 +0530

    #03 show angular app in iframe within react

---
+++ 03-angular-in-react-using-iframe/src/components/ReactComponent/ReactComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ReactComponent.css";
+
+function ReactComponent() {
+  return (
+    <div className="ReactComponent">
+      <div>React Component</div>
+      <iframe src="/angular-sample-app/index.html" width="800" height="500" />
+    </div>
+  );
+}
+
+export default ReactComponent;
