e4dbb0a1ad09790e8ef48d411f5ecc432882e2ac
Souvik Basu <souvikbasu@gmail.com>
2019-05-22 09:55:34 +0530

    add vue sample app

---
+++ vue-sample-app/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <div>Vue App</div>
+    <VueComponent/>
+  </div>
+</template>
+
+<script>
+import VueComponent from "./components/VueComponent.vue";
+
+export default {
+  name: "app",
+  components: {
+    VueComponent
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  padding: 40px;
+  background-color: rgb(220, 223, 219);
+}
+
+.vue-html-text {
+  font-size: 2rem;
+  margin-bottom: 20px;
+}
+
+#app {
+  display: block;
+  height: 500px;
+  width: 500px;
+  background-color: #2c3e50;
+  padding: 20px;
+  font-size: 2rem;
+  color: white;
+  border: solid 2px darkgray;
+}
+</style>
---
+++ vue-sample-app/src/components/VueComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>Vue Component</div>
+</template>
+
+<script>
+export default {
+  name: "VueComponent",
+  props: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  display: block;
+  height: 300px;
+  width: 300px;
+  background-color: #42b983;
+  padding: 20px;
+  font-size: 2rem;
+  color: white;
+  border: solid 2px darkgray;
+  margin: 20px;
+}
+</style>
